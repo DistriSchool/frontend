cd983116ba4f37d08f2a94a6d4e6429377844e24
clys-man <clysmanalves@gmail.com>
2025-11-24 16:57:31 -0300

    feat: remove register page

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -108,27 +108,6 @@ const Login = () => {
                     <Button className="w-full flex justify-center items-center ">Login</Button>
                 </div>
             </form>
-
-            <div className="mt-6">
-                <div className="relative">
-                    <div className="absolute inset-0 flex items-center">
-                        <div className="w-full border-t border-gray-300"></div>
-                    </div>
-                    <div className="relative flex justify-center text-sm">
-                        <span className="px-2 bg-white text-gray-500">
-                            Don't have an account?
-                        </span>
-                    </div>
-                </div>
-
-                <div className="mt-6">
-                    <Link
-                        href="/register"
-                        className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Create an account
-                    </Link>
-                </div>
-            </div>
         </>
     )
 }
